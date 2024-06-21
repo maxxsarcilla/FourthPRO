3d7af81140afbc7935a6cd69ff9b47f7560f109c
unknown <maxxsarcilla@gmail.com>
2024-06-21 14:18:15 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int [] numbersTocheck = {1,2,3,4,5,6,7,8,9,10};
+
+foreach(int number in numbersTocheck)
+{
+    if (number % 2 == 0)
+{
+    Console.WriteLine($"Number {number} is even!");
+}
+
+    else
+{
+    Console.WriteLine($"Number {number} is not even!");
+}
+}
